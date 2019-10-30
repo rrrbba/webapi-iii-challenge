2d0c9eb910da4c35829b437c50a804f1312dd8a2
Rayven <rayven.burns@gmail.com>
2019-10-30 17:25:11 -0400

    mvp done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server');
+
+server.listen(7000, () => {
+    console.log('\n***COME TO 7000 IF YOU DARE***\n');
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,136 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const db = require('./userDb');
+const postdb = require('../posts/postDb');
 
+
+router.post('/', validateUser, (req, res) => {
+    const newUser = req.body;
+
+    db.insert(newUser)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "POST FAILED!" })
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
+    const newPost = req.body;
 
+    postdb.insert(newPost)
+    .then(post => {
+        res.status(200).json(post);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "POST FAILED!" })
+    })
 });
 
 router.get('/', (req, res) => {
-
+    db.get()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "GET FAILED!"})
+        })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
 
+    db.getById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "GET FAILED!"})
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
+    const id = req.params.id;
 
+    db.getUserPosts(id)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "GET FAILED!"})
+    })
+    
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
 
+    db.getById(id) //find the user by id
+    .then(user => {
+        db.remove(id) //then remove said user
+        .then(list => { //the user is removed from the list of users
+            res.status(200).json(user); //return the deleted user
+        })
+        .catch(err => {
+            res.status(500).json({ error: "DELETE FAILED!"})
+        })
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    const newPost = req.body;
 
+        db.update(id, newPost)
+        .then(update => {
+            res.status(200).json(update);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "UPDATE FAILED!"})
+        })
+    
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const id = req.params.id;
 
+    db.getById(id)
+    .then(user => {
+        if(user){ //if theres an id
+            next();
+        } else {
+            res.status(404).json({ message: "GIVE ME AN ID!" })
+        }
+    })
+    .catch(err => {
+        res.status(400).json({ message: "Invalid user id!"})
+    })
 };
 
 function validateUser(req, res, next) {
-
+    if(!req.body){
+        res.status(400).json({ message: "Missing user data."})
+    } else if (!req.body.name){
+        res.status(400).json({ message: "Missing required name field"});
+    } else {
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
-
+    if(!req.body){
+        res.status(400).json({ message: "Missing post data."})
+    } else if (!req.body.name){
+        res.status(400).json({ message: "Missing required text field"});
+    } else {
+        next();
+    }
 };
+
 
 module.exports = router;
